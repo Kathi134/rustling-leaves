d23d499e8f276c4d882eed9b4973f6aaea14c54a
Katharina Reiner <kathi-reiner@gmx.de>
2026-01-08 22:15:42 +0100

    fix: correct use of TickableCondition for AreaInclusion Field Types

--- backend/rustlingLeaves/src/main/kotlin/kpdev/rustlingleaves/model/skeleton/FieldType.kt
+++ backend/rustlingLeaves/src/main/kotlin/kpdev/rustlingleaves/model/skeleton/FieldType.kt
@@ -26,7 +26,7 @@ enum class FieldType(
     BIRD_RIGHT(FactoringPairWithTypeStrategy(5, BIRD_LEFT)),
     RAINBOW(AllTickedScoreStrategy(30)),
     SEED(SeedAndCloudScoreStrategy()),
-    BEAR(AreaInclusionScoreStrategy(false, 3)),
+    BEAR(AreaInclusionScoreStrategy(false, 3), NotTickableCondition()),
 
     TREE_SUMMER(FactoringScoreStrategy(2), DiceValueCondition(DiceResult.CLOUD_VALUES)),
     DAISY(OddFactoringScoreStrategy(2)),
@@ -35,8 +35,8 @@ enum class FieldType(
     FRUIT_CHERRY, // MultiplyWithTypesScoreStrategy(setOf(FRUIT_APPLE, FRUIT_CHERRY, FRUIT_PEACH))
     FRUIT_PEACH(MultiplyWithTypesScoreStrategy(setOf(FRUIT_APPLE, FRUIT_CHERRY, FRUIT_PEACH))),
     FIREFLY(FactoringScoreStrategy(3), DiceValueCondition(DiceResult.NONE_CLOUD_VALUES)),
-    MOSQUITO(AllAreaInclusionScoreStrategy(false, 15)),
-    SNAKE(AreaInclusionScoreStrategy(false, 3)),
+    MOSQUITO(AllAreaInclusionScoreStrategy(false, 15), NotTickableCondition()),
+    SNAKE(AreaInclusionScoreStrategy(false, 3), NotTickableCondition()),
 
     MUSHROOM_TOADSTOOL(FactoringScoreStrategy(-2), NotTickableCondition()),
     MUSHROOM_PORCINI(FactoringScoreStrategy(3), tickGroup = TickGroup(MUSHROOM_TOADSTOOL)),
