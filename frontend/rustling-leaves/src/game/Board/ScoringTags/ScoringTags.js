fdd9af7e299cd298fc5039d078fc1463410737c3
Katharina Reiner <kathi-reiner@gmx.de>
2026-01-13 11:19:05 +0100

    feat: very raw scoring tags component

--- frontend/rustling-leaves/src/game/Board/ScoringTags/ScoringTags.js
+++ frontend/rustling-leaves/src/game/Board/ScoringTags/ScoringTags.js
@@ -1,7 +1,19 @@
+import "./scoring-tags.css"
+import { useEffect } from "react"
+
+
 export default function ScoringTags({ tags }) {
+
+    useEffect(() => console.log(tags), [tags])
+
     return (
-        <div>
-            TBD: ScoringTags component - display scoring tags based on the player's card data.
+        <div className="horizontal-container center">
+            <table><tbody>
+                {tags?.sort((a,b) => a.fieldTypeDto.enumName.localeCompare(b.fieldTypeDto.enumName)).map((x, i) => <tr>
+                    <td className="scoring-tag-img" key={i} style={{backgroundImage: `url("${process.env.PUBLIC_URL}/field-types/${x.fieldTypeDto.enumName.toLocaleLowerCase()}.jpg")`}} />
+                    <td>{x.value} / {x.max} </td>
+                </tr>)}
+            </tbody></table>
         </div>
     )
 }
